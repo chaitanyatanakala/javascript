07cdd16c04e0e5ba82742266133074f4d6f13768
chaitanyatanakala <chaitanyatanakala@gmail.com>
2024-09-30 16:48:38 +0530

    ye

---
+++ task3.js
@@ -0,0 +1,60 @@
+// map-->
+// let arr=[1,2,3,4,5];
+// let x=arr.map((a,b)=>{return a+a})
+// console.log(x)
+
+//  let str=["hello","world"];
+//  let x=str.map((a,b)=>{return a.toUpperCase()})
+//  console.log(x)
+ 
+//  filter-->
+// let arr=[1,2,3,4,5];
+// let x=arr.filter((a,b)=>a%2==0)
+// console.log(x)
+
+// let str=["hello","world","abc"]
+// let x=str.filter((a,b)=>{return a.length>=5})
+// console.log(x)
+
+// for each
+// let arr=[1,2,3,4,5];
+// arr.forEach((a,b)=>{console.log(a)})
+
+// let str=["hello","world"];
+//     str.forEach((a)=>{console.log(a+"!")})
+
+// every
+// let arr=[1,2,3,4,5];
+// let x=arr.every((a,b,c)=>{return a>0})
+// console.log(x)
+
+// let str=["hello","world","max"];
+// let x=str.every((a,b,c)=>{return a.length>=5})
+// console.log(x)
+
+// some
+// let arr=[1,2,3,4,5];
+// let x=arr.some((a,b)=>a%2==0)
+// console.log(x)
+
+// let str=["hello","world","max"]
+// let x=str.some((a,b,c)=>{return a.includes("x")})
+// console.log(x)
+
+// find
+// let arr=[1,2,3,4,5];
+// let x=arr.find((a,b)=>a%2==0)
+// console.log(x)
+
+// let str=["hello","world","max"]
+// let x=str.find((a,b,c)=>{return a.includes("x")})
+// console.log(x)
+
+// find index
+// let arr=[1,2,3,4,5];
+// let x=arr.findIndex((a,b)=>a%2==0)
+// console.log(x)
+
+// let str=["hello","world",",max"];
+// let x=str.findIndex((a,b)=>{return a.includes("x")})
+// console.log(x)
