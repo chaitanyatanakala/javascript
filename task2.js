3889b543721f751d27ac2b3822185f65e80abff6
chaitanyatanakala <chaitanyatanakala@gmail.com>
2024-09-30 16:43:39 +0530

    yes

---
+++ task2.js
@@ -0,0 +1,100 @@
+
+
+// ---->test cases<-----
+// ---->sum of all even numbers
+// var sum=0
+// for(let i=1;i<=100;i++){
+//     if(i%2==0){
+//         sum=sum+i;
+// }
+// }
+// console.log(" the sum of even numbers between 1 to 100 is: "+sum);
+
+// var sum=0
+// for(let i=1;i<=50;i++){
+//     if(i%2==0){
+//         sum=sum+i;
+// }
+// }
+// console.log(" the sum of even numbers between 1 to 50 is: "+sum);
+
+// print factorial of a number
+// function factorial(n){
+//     if(n==0){
+//         return 1
+//     }
+//     else{
+//         return n*factorial(n-1)
+//     }
+// }
+// console.log(factorial(5)
+
+
+// ---->Else-if ladder<-----
+// ---->fare of taxi ride
+// let distance=0;
+// if(distance>0 && distance<=5){
+//     console.log("fare is $5");
+// }
+// else if(distance>5 && distance<=10){
+//     console.log("fare is $10");
+// }
+// else if(distance>10 && distance<=20){
+//     console.log("fare is $20");
+// }
+// else if(distance>20){
+//     console.log("fare is $30 + 5$ extra for every extra km")
+// }
+// else{
+//     console.log("NO ride ")
+// }
+
+
+// ---->discounts of products
+// let units=0;
+// if(units>0 && units<=5){
+//      console.log("no discount on your purchase")
+//  }
+//  else if(units>=6 && units<=10){
+//      console.log("thank for your purchase you have 5% discount on your purchase")
+//  }
+//  else if(units>=11 && units<=20){
+//     console.log("thank for your purchase you have 10% discount on your purchase")
+// }
+// else if(units>=20){
+//     console.log("thank for your purchase you have 15% discount on your purchase")
+//  }
+//  else{
+//      console.log("invalid purchase")
+// }
+
+
+
+// --> switch statements--->
+// ---->coffe selection
+// let coffe="latte";
+// switch(coffe){
+//     case "espresso":console.log('espresso is ready');
+//     break;
+//     case "cappuccino":console.log('cappuccino is ready');
+//     break;
+//     case "latte":console.log('latte is ready');
+//     break;
+//     case "mocha":console.log('mocha is ready');
+//     break;
+// }
+
+// ---->shipping cost
+// let country="australia";
+// switch(country){
+//     case "northamerica":console.log('north america shipping cost is $10');
+//     break;
+//     case "southamerica":console.log('southamerica shipping cost is $20');
+//     break;
+//     case "europe":console.log('europe shipping cost is $30');
+//     break;
+//     case "asia":console.log(' asia shipping cost is $40');
+//     break;
+//     case "australia":console.log('australia shipping cost is $50');
+//     break;
+// }
